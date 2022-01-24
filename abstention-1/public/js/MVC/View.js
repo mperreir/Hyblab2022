cedd83d6eee9d83396bc14bd155366d3b5babb03
Kilily <aurelie.boulais@gmail.com>
2022-01-24 20:55:15 +0100

    Scroll sur html et css, + view.js + interface fixe.

---
+++ abstention-1/public/js/MVC/View.js
@@ -0,0 +1,16 @@
+
+class View {
+
+    constructor(){
+    
+        this.dialoguePart=document.querySelector('#Dialogue');
+        this.transitionPartPart=document.querySelector('#Transition');
+
+        this.perso1=document.querySelector('#personnage1');
+        this.perso2=document.querySelector('#personnage2');
+        this.textZone=document.querySelector('#textZone');
+
+        this.animation= document.querySelector('#animation');
+    }
+  }
+  
