79ee7eb9b2be38b2e701ed484c4625816bf83d29
Aurelien B <61365539+Neltab@users.noreply.github.com>
2022-01-25 22:35:41 +0100

    Delete test.js

--- abstention-1/public/js/MVC/cssProperties/test.js
+++
@@ -1,7 +0,0 @@
-const Transform = require("./Transform");
-
-let test1 = new Transform().translate(10,12).scale(5,6).rotate(90);
-
-let test2 = new Transform().translate(0,0).scale(0,0).rotate(0);
-
-console.log(test1.interpolateWith(test2, 0.5));
