eb13d0ecbfd42cbf124896dbfa77648b0dc5170e
Rémi Audoin <remi.audoin@etu.univ-nantes.fr>
2022-01-25 11:44:24 +0100

    add a first version of connecting lines

---
+++ abstention-2/public/js/lineConnecting.js
@@ -0,0 +1,71 @@
+const containerDiv = document.getElementById("container");
+const containerBoundingRect = containerDiv.getBoundingClientRect();
+
+for (let i = 0; i < proofTarget.length - 1; i++) {
+    const child = proofTarget[i];
+
+    let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    let line = document.createElementNS("http://www.w3.org/2000/svg", 'line');
+
+    let boundingRect = child.getBoundingClientRect();
+    console.debug(boundingRect);
+    let nextChildBoundingRect = proofTarget[i + 1].getBoundingClientRect();
+
+    let x1, x2, y1, y2;
+
+    if (nextChildBoundingRect.left > boundingRect.right) {
+        x1 = boundingRect.right - containerBoundingRect.left;
+        // y1 = boundingRect.top + (boundingRect.bottom - boundingRect.top) / 2
+        // } else if (nextChildBoundingRect.right < boundingRect.left) {
+        //     x1 = boundingRect.left - containerBoundingRect.left;
+    } else {
+        x1 = (boundingRect.left + (boundingRect.right - boundingRect.left) / 2) - containerBoundingRect.left;
+    }
+
+    if (nextChildBoundingRect.top > boundingRect.bottom) {
+        y1 = boundingRect.bottom - containerBoundingRect.top;
+        // y1 = boundingRect.top + (boundingRect.bottom - boundingRect.top) / 2
+    } else if (nextChildBoundingRect.bottom < boundingRect.top) {
+        y1 = boundingRect.top - containerBoundingRect.top;
+    } else {
+        y1 = boundingRect.top + (boundingRect.bottom - boundingRect.top) / 2
+    }
+
+    if (nextChildBoundingRect.left > boundingRect.right) {
+        x2 = nextChildBoundingRect.left - containerBoundingRect.left;
+        // y1 = boundingRect.top + (boundingRect.bottom - boundingRect.top) / 2
+        // } else if (nextChildBoundingRect.right < boundingRect.left) {
+        //     x1 = boundingRect.left - containerBoundingRect.left;
+    } else {
+        x2 = (nextChildBoundingRect.left + (nextChildBoundingRect.right - nextChildBoundingRect.left) / 2) - containerBoundingRect.left;
+    }
+
+    if (nextChildBoundingRect.top > boundingRect.bottom) {
+        y2 = nextChildBoundingRect.top - containerBoundingRect.top;
+    } else if (nextChildBoundingRect.bottom < boundingRect.top) {
+        y2 = nextChildBoundingRect.bottom - containerBoundingRect.top;
+    } else {
+        y2 = nextChildBoundingRect.top + (nextChildBoundingRect.bottom - nextChildBoundingRect.top) / 2
+    }
+
+
+    // x2 = (nextChildBoundingRect.left + (nextChildBoundingRect.right - nextChildBoundingRect.left) / 2) - containerBoundingRect.left;
+    // y2 = nextChildBoundingRect.bottom;
+    line.setAttribute("stroke", "red");
+    line.setAttribute("x1", x1);
+    line.setAttribute("y1", y1);
+    line.setAttribute("x2", x2);
+    line.setAttribute("y2", y2);
+    line.setAttribute("stroke", "red");
+    line.setAttribute("stroke-width", "20");
+    line.classList.add("panning-animated");
+
+    svg.style.position = "absolute";
+    svg.style.filter = "drop-shadow(3px 5px 2px rgb(215 215 215 / 70%))"
+    svg.appendChild(line);
+
+
+    svg.style.width = "1000vh";
+    svg.style.height = "1000vh";
+    document.getElementById("container").appendChild(svg);
+}
--- abstention-2/public/js/panning.js
+++ abstention-2/public/js/panning.js
@@ -6,7 +6,7 @@ nextBtn.addEventListener('click', next);
 const previousBtn = document.getElementById('previousBtn');
 previousBtn.addEventListener('click', previous);
 
-const trajectory = [{ in: [-200, 50] }, { in: [-100, -140] }]
+const trajectory = [{ in: [-85, 50] }, { in: [0, -200] }, { in: [-110, -140] }, { in: [-160, -80] }, { in: [-80, -60] }]
 
 const proofTarget = document.getElementsByClassName('proof');
 for (let i = 1; i < proofTarget.length; i++) {
@@ -44,7 +44,7 @@ function previousAnimation() {
 function nextAnimation(trajectory) {
     switchButton();
     let anim = anime({
-        targets: document.getElementsByClassName('proof'),
+        targets: document.getElementsByClassName('panning-animated'),
         translateX: trajectory[animQueue.length].in[0] + 'vw',
         easing: 'easeInOutQuad',
         duration: 2000,
@@ -64,3 +64,21 @@ function switchButton() {
     previousBtn.disabled = !previousBtn.disabled;
     nextBtn.disabled = !nextBtn.disabled;
 }
+
+// let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+// // svg.setAttribute("width", "100vw");
+// // svg.setAttribute("height", "100");
+// let line = document.createElementNS("http://www.w3.org/2000/svg", 'line');
+
+// line.setAttribute("stroke", "red");
+// line.setAttribute("x1", "0");
+// line.setAttribute("y1", "0");
+// line.setAttribute("x2", "50");
+// line.setAttribute("y2", "10vw");
+// line.setAttribute("stroke", "red");
+// line.setAttribute("stroke-width", "20");
+// svg.style.position = "absolute";
+// svg.appendChild(line);
+
+
+// document.getElementById("container").appendChild(svg);
