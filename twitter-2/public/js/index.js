d4394747a89f3c099101d1e9322caf5c1ae20f1f
François <francois.charpentier1@etu.univ-nantes.fr>
2022-01-26 17:35:03 +0100

    premiere page

--- twitter-2/public/js/index.js
+++ twitter-2/public/js/index.js
@@ -9,21 +9,12 @@ const swiper = new Swiper("#mySwiper", {
   },
 });
 
-// Wait for the video to preload and display 1st slide
-const video = videojs(document.querySelector('#background-video'));
-video.one('loadeddata', (event) => { 
-  // fade out the loader "slide"
-  // and send it to the back (z-index = -1)
-  anime({
-    delay: 1000,
-    targets: '#loader',
-    opacity: '0',
-    'z-index' : -1,
-    easing: 'easeOutQuad',
-  });
-  // Init first slide
-  initSlide1();
-  // Debug trace because the loadeddata event is
-  // sometime not fired
-  console.log("Video loaded");
+anime({
+  delay: 1000,
+  targets: '#loader',
+  opacity: '0',
+  'z-index' : -1,
+  easing: 'easeOutQuad',
 });
+// Init first slide
+initSlide1();
--- twitter-2/public/js/slide1.js
+++ twitter-2/public/js/slide1.js
@@ -3,19 +3,10 @@
 // async init function (because of the awaits on fetches)
 const initSlide1 = async function(){
 
-  // Animate hyblab logo and make shrink on click
-  anime({
-    targets: '#logo-hyblab',
-    scale: '1.2',
-    easing: 'easeInOutQuad',
-    direction: 'alternate',
-    loop: true
-  });
-
-  const logo = document.querySelector('#logo-hyblab');
-  logo.addEventListener('click', () => {
+  let vector = document.querySelector('#vector');
+  vector.addEventListener('click', () => {
     anime({
-        targets: '#logo-hyblab',
+        targets: '#vector',
         scale: 0
       });
     swiper.slideNext()
