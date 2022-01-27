2997c5027de1017927af659a5210609b8697deed
RomainG <romain.gentilhomme@etu.univ-nantes.fr>
2022-01-27 19:57:04 +0100

    management of sections and buttons

--- q-a-1/public/js/informer.js
+++ q-a-1/public/js/informer.js
@@ -3,11 +3,11 @@
 const footer = document.querySelector('footer');
   
 const button_first_choice = document.createElement('button');
-button_first_choice.textContent = "Retour";
+button_first_choice.textContent = "PROCURATION";
 button_first_choice.classList.add("blue");
   
 const button_second_choice = document.createElement('button');
-button_second_choice.textContent = "Suivant";
+button_second_choice.textContent = "BUREAU DE VOTE";
 button_second_choice.classList.add("blue");
   
   
