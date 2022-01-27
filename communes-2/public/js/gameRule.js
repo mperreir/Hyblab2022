339cee86e680dbf67d2c7e7053206260d4853c71
Mohamed Berté <mohamed-ibrahim.berte@etu.univ-nantes.fr>
2022-01-27 12:31:43 +0100

    OK

--- communes-2/public/js/gameRule.js
+++ communes-2/public/js/gameRule.js
@@ -1,12 +1,37 @@
-<<<<<<< HEAD
-page('/communes-2/gamerule', async function () {
-=======
 page('/communes-2/gameRule', async function () {
->>>>>>> a290ac0fd26e86fe17055241192824b968c63590
     await renderTemplate(templates('./templates/gameRule.mustache'));
 
     const Retourbtn = document.getElementById("boutonRetour");
     Retourbtn.addEventListener('click', function () {
         page('/communes-2/');
     });
+    showSlides(1);
+
 });
+
+var slideIndex = 1;
+
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("tips-content");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("active", "");
+      }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
