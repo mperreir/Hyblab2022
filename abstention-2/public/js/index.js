90fd0c432d0588ee3da56078a9cf27c0fdd5697f
Rémi Audoin <remi.audoin@etu.univ-nantes.fr>
2022-01-27 22:39:45 +0100

    implement file explorer page (without interaction)

--- abstention-2/public/js/index.js
+++ abstention-2/public/js/index.js
@@ -1,1 +1,17 @@
 "use strict";
+
+
+const test = {
+    data: "hello"
+};
+
+loadFileExplorer();
+
+async function loadTemplate(path, data) {
+    const fileExplorerTemplate = await fetch(path);
+    const fileExplorerHtml = await fileExplorerTemplate.text();
+    return ejs.render(fileExplorerHtml, data);
+}
+
+
+
