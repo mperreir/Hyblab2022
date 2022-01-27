7df663b8790ae395570379c5c4610a4a20fdf124
axel <axel.rocheteau4@gmail.com>
2022-01-27 15:51:36 +0100

    api

--- abstention-2/api/api.js
+++ abstention-2/api/api.js
@@ -6,9 +6,16 @@ const fs = require('fs');
 
 // Sample endpoint that sends the partner's name
 app.get('/city/:city', function (req, res) {
-    const file = fs.readFileSync(path.join(__dirname, 'cities.json'));
+    const file = fs.readFileSync(path.join(__dirname, 'data.json'));
     const cityData = JSON.parse(file)[req.params.city];
     res.status(cityData ? 200 : 400).json(cityData || { 'error': true, 'message': 'Unknown city.' });
+});
+
+app.get('/national', function (req, res) {
+    const file = fs.readFileSync(path.join(__dirname, 'france.json'));
+    const franceData = JSON.parse(file);
+    console.log(franceData);
+    res.status(franceData ? 200 : 400).json(franceData || { 'error': true, 'message': 'Unknown city.' });
 });
 
 // Export our API
