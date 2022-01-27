b03c3c391c2d1f95f559d4bf2eb5946a6eda88d2
Victorien Plot <victorien.plot@etu.univ-nantes.fr>
2022-01-27 16:52:14 +0100

    Template folder-absention et ajout font CabinetGrotesk

--- abstention-2/public/js/index.js
+++ abstention-2/public/js/index.js
@@ -1,1 +1,8 @@
 "use strict";
+
+const templateTest = fetch('templates/folder-absention.ejs').then(value => {
+    console.log(value.text().then(value => {
+        const rendered = ejs.render(value, { votingHab: 100000 });
+        document.getElementById('container').innerHTML = rendered;
+    }));
+});
