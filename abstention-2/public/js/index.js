d20ee38424caecac61f39ef8f7a85c53894468fa
Felix R <felix@rabadan.fr>
2022-01-27 15:09:56 +0100

    add header sms page

--- abstention-2/public/js/index.js
+++ abstention-2/public/js/index.js
@@ -1,1 +1,15 @@
 "use strict";
+const container = document.getElementById("container");
+
+const test = {
+    data: "hello"
+};
+loadTemplate('templates/sms.ejs', test).then(value => { container.innerHTML = value ;
+date();
+})
+
+async function loadTemplate(path, data) {
+    const fileExplorerTemplate = await fetch(path);
+    const fileExplorerHtml = await fileExplorerTemplate.text();
+    return ejs.render(fileExplorerHtml, data);
+}
