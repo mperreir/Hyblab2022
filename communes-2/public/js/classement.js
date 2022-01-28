2ff20473c525f4e4a606f2d7347788aa874098aa
Alexandre <alexandre.clouet1@etu.univ-nantes.fr>
2022-01-28 23:48:46 +0100

    page classement

--- communes-2/public/js/classement.js
+++ communes-2/public/js/classement.js
@@ -1,3 +1,10 @@
 page('/communes-2/classement', async function () {
     await renderTemplate(templates('./templates/classement.mustache'));
+
+    const rePlaybtn = document.getElementById("save-btn");
+    rePlaybtn.addEventListener('click', function () {
+        pseudo = document.getElementById("pseudo").value
+        console.log("Pseudo du joueur : "+pseudo)
+        page('/communes-2/gameChoice');
+    });
 });
