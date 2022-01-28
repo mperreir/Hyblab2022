9c0b906c7c940e52b84cebd9d039f03709babce9
Gabriel JOLLY <gabriel-jolly@orange.fr>
2022-01-28 16:52:30 +0100

    ajout question 6

--- communes-1/public/js/question.js
+++ communes-1/public/js/question.js
@@ -1,9 +1,11 @@
 'use strict';
 let data_election = require('./chargement.js');
 data_election = data_election.data_election
+
+
 class Question{
-    constructor(commune, annee) {
-        this.commune = commune;
+    constructor(code_commune, annee) {
+        this.code_commune = code_commune;
         this.annee = annee;
     }
     generate_question()
@@ -13,8 +15,8 @@ class Question{
 
 
 class More_vote_second_tour extends Question{
-    constructor(commune, annee) {
-        super(commune, annee);
+    constructor(code_commune, annee) {
+        super(code_commune, annee);
     }
     generate_question()
     {
@@ -24,8 +26,42 @@ class More_vote_second_tour extends Question{
         let vote =  data_election.get(key).get(7).voix;
         let answer1 = [prenom[0] + " " + nom[0], vote[0] > vote[1]];
         let answer2 = [prenom[1] + " " + nom[1], vote[1] > vote[0]];
-        return ["À " + data_election.get(key).get(this.commune).nom_com + ", qui a obtenu le plus de voix au second tour en " + this.annee + " ?",answer1,answer2];
+        return ["À " + data_election.get(key).get(this.code_commune).nom_com + ", qui a obtenu le plus de voix au second tour en " + this.annee + " ?",answer1,answer2];
     }
 }
 
-console.log(new More_vote_second_tour(7,"2002").generate_question())
+class Question6 extends Question {
+    constructor(code_commune, annee, valeur) {
+        super(code_commune, annee);
+        this.valeur = valeur;
+    }
+    generate_question() {
+        let key = this.annee + "_2";
+        let info_commune = data_election.get(key).get(this.code_commune);
+        let nom_commune = info_commune.nom_com;
+
+        let question = "À " + nom_commune + ", le taux de participation est-il " +
+            "supérieur à " + this.valeur + "% en " + this.annee + " ?";
+
+        let reponse1 = new Array(2);
+        reponse1.push("Oui");
+        reponse1.push(true);
+
+        let reponse2 = new Array(2);
+        reponse2.push("Non");
+        reponse2.push(false);
+
+        let res = new Array(3)
+        res.push(question);
+        res.push(reponse1);
+        res.push(reponse2);
+
+        return res;
+    }
+
+
+}
+
+
+console.log(new More_vote_second_tour(7,"2002").generate_question());
+console.log(new Question6(7,"2002",30).generate_question());
