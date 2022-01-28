b4eeeb38ce01c1824f27e786a6b99b15ce9f8cfb
Celian Rolland <celian.rollanc@gmail.com>
2022-01-28 14:51:41 +0100

    Chargement des données améliorer

--- communes-1/public/js/chargement.js
+++ communes-1/public/js/chargement.js
@@ -16,20 +16,23 @@ const map2012_2 = new Map();
 const map2017_1 = new Map();
 const map2017_2 = new Map();
 
-jsonData.forEach(row => map2002_1.set(row.code,row))
-jsonData2.forEach(row => map2002_2.set(row.code,row))
-jsonData3.forEach(row => map2007_1.set(row.code,row))
-jsonData4.forEach(row => map2007_2.set(row.code,row))
-jsonData5.forEach(row => map2012_1.set(row.code,row))
-jsonData6.forEach(row => map2012_2.set(row.code,row))
-jsonData7.forEach(row => map2017_1.set(row.code,row))
-jsonData8.forEach(row => map2017_2.set(row.code,row))
+const data = new Map();
+jsonData.forEach(row => map2002_1.set(row.code,row));
+jsonData2.forEach(row => map2002_2.set(row.code,row));
+jsonData3.forEach(row => map2007_1.set(row.code,row));
+jsonData4.forEach(row => map2007_2.set(row.code,row));
+jsonData5.forEach(row => map2012_1.set(row.code,row));
+jsonData6.forEach(row => map2012_2.set(row.code,row));
+jsonData7.forEach(row => map2017_1.set(row.code,row));
+jsonData8.forEach(row => map2017_2.set(row.code,row));
 
-export {map2007_1};
-export {map2007_2};
-export {map2002_1};
-export {map2002_2};
-export {map2017_1};
-export {map2017_2};
-export {map2012_1};
-export {map2012_2};
+data.set(2002_1,map2002_1);
+data.set(2002_2,map2002_2);
+data.set(2007_1,map2007_1);
+data.set(2007_2,map2007_2);
+data.set(2012_1,map2012_1);
+data.set(2012_2,map2012_2);
+data.set(2017_1,map2017_1);
+data.set(2017_2,map2017_2);
+
+export {data};
