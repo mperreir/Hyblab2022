56764c248c7d88137294940ba196c098494aa88f
Thomas Clouet <thomas.clouet1@etu.univ-nantes.fr>
2022-01-29 16:43:57 +0100

    :bug: Résolution virtual keyboard

--- communes-1/public/js/js_anjoute-choix-commune.js
+++ communes-1/public/js/js_anjoute-choix-commune.js
@@ -1,18 +1,4 @@
 'use strict';
 
-document.documentElement.style.setProperty('overflow', 'auto');
-
-const metaViewport = document.querySelector('meta[name=viewport]');
-const body = document.querySelector('body');
-
-var test =  body.offsetHeight;
-
-console.log(test);
-
-
-let testttt = () => {
-	metaViewport.setAttribute('content', 'height=' + test + 'px, width=device-width, initial-scale=1.0');
-	console.log('+' + test);
-}
-
-window.onresize = testttt;
+var viewport = document.querySelector("meta[name=viewport]");
+viewport.setAttribute("content", viewport.content + ", height=" + window.innerHeight);
