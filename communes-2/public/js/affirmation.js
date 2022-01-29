ef53eda6aa97d817d7885c32ff24bb15ec217db6
Mohamed Berté <mohamed-ibrahim.berte@etu.univ-nantes.fr>
2022-01-29 10:04:11 +0100

    Test

--- communes-2/public/js/affirmation.js
+++ communes-2/public/js/affirmation.js
@@ -1,5 +1,101 @@
 'use strict'
 
+
+
+'use strict'
+
+page('/communes-2/affirmation', async function () {
+    await renderTemplate(templates('./templates/affirmation.mustache'));
+
+
+    let response = await fetch('api/carte');
+    const dataCarte = await response.json();
+    console.log(dataCarte);
+
+
+    var map = L.map('map').setView([46.87,-1.64], 8);
+    // On affiche la map google maps derrière.
+    var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 18,
+        /*attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',*/
+        id: 'mapbox/light-v9',
+        tileSize: 512,
+        zoomOffset: -1
+    }).addTo(map);
+    // On ajoute le fichier GEOJson comme une couche.
+    L.geoJson(dataCarte, {
+        // Plusieurs paramètres pour pouvoir modifier ce qu'on affiche, la manière dont on l'affiche, etc.
+        // Voir ici les différentes possiblités pour les options : https://leafletjs.com/examples/geojson/
+
+        // Changement de la couleur en fonction de la propriété "orientation"
+        style : function(feature, layer) {
+            switch (feature.properties.orientation) {
+                case "Centre": return {color: "#00ff00"};
+                case "Droite": return {color: "#0000ff"};
+                case "Gauche": return {color: "#ff0000"};
+            }
+        },
+        // Affichage des villes qui ont seulement la proprité orientation à "Gauche" ou à "Droite"
+        filter: function(feature, layer) {
+            return feature.properties.orientation == "Gauche" || feature.properties.orientation == "Droite";
+        },
+        // Pour chaque ville.
+        onEachFeature: function(feature, layer) {
+            // Voir ici les différentes possibilités pour chaque layer : https://leafletjs.com/reference.html#layer
+
+            // Affichage d'un popup avec la propriété "nom" quand on click dessus.
+            layer.bindPopup(feature.properties.nom);
+            // Lors d'un click, on appelle la fonction "whenClicked"
+            layer.on({
+                click: whenClicked
+            });
+        }
+    }).addTo(map);
+
+    function whenClicked(e) {
+        console.log(e.target.feature.properties.nom);
+    }
+
+
+
+    const Retourbtn = document.getElementById("boutonRetour");
+    Retourbtn.addEventListener('click', function () {
+        page('/communes-2/gameChoice');
+        showSlides(1);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 var color_droite = "#5B6C9A";
 var color_centre = "#83C49E";
 var color_gauche = "#ED6464";
@@ -185,4 +281,4 @@ function zoom(scale) {
 function setMatrix() {
     var newMatrix = "matrix(" +  transformMatrix.join(' ') + ")";
     document.getElementById("geojson").setAttributeNS(null, "transform", newMatrix);
-}
+}*/
