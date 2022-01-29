6cb06fc78d90ee72ae074670322ee0fb913c6026
Thomas Clouet <thomas.clouet1@etu.univ-nantes.fr>
2022-01-29 16:54:42 +0100

    :zap: Passage en simple quote

--- communes-1/public/js/js_anjoute-choix-commune.js
+++ communes-1/public/js/js_anjoute-choix-commune.js
@@ -1,4 +1,4 @@
 'use strict';
 
-var viewport = document.querySelector("meta[name=viewport]");
-viewport.setAttribute("content", viewport.content + ", height=" + window.innerHeight);
+var viewport = document.querySelector('meta[name=viewport]');
+viewport.setAttribute('content', viewport.content + ', height=' + window.innerHeight);
