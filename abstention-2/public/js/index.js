f64fd4bb13cc6d214d8002648cac1b053f810941
Victorien Plot <victorien.plot@etu.univ-nantes.fr>
2022-01-29 17:39:28 +0100

    Ajout données pour page folder

--- abstention-2/public/js/index.js
+++ abstention-2/public/js/index.js
@@ -2,21 +2,21 @@
 
 const templateTest = fetch('templates/folder-absention.ejs').then(value => {
     console.log(value.text().then(value => {
-        const rendered = ejs.render(value, { votingHab: 100000 });
+        const rendered = ejs.render(value, { votingHab: 100000, pcVotingHab: 35, pcMean: 30 });
         document.getElementById('container').innerHTML = rendered;
     }));
 });
-const container = document.getElementById("container");
+// const container = document.getElementById("container");
 
-const test = {
-    data: "hello"
-};
-loadTemplate('templates/sms.ejs', test).then(value => { container.innerHTML = value ;
-date();
-})
+// const test = {
+//     data: "hello"
+// };
+// loadTemplate('templates/sms.ejs', test).then(value => { container.innerHTML = value ;
+// date();
+// })
 
-async function loadTemplate(path, data) {
-    const fileExplorerTemplate = await fetch(path);
-    const fileExplorerHtml = await fileExplorerTemplate.text();
-    return ejs.render(fileExplorerHtml, data);
-}
+// async function loadTemplate(path, data) {
+//     const fileExplorerTemplate = await fetch(path);
+//     const fileExplorerHtml = await fileExplorerTemplate.text();
+//     return ejs.render(fileExplorerHtml, data);
+// }
