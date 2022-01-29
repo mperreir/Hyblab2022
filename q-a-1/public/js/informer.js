dceefb2d4dcbd97e9fd03abea639073a376a153a
RomainG <romain.gentilhomme@etu.univ-nantes.fr>
2022-01-29 14:17:11 +0100

    creatiion of the credit page

--- q-a-1/public/js/informer.js
+++ q-a-1/public/js/informer.js
@@ -9,8 +9,8 @@ button_first_choice.classList.add("blue");
 const button_second_choice = document.createElement('button');
 button_second_choice.textContent = "BUREAU DE VOTE";
 button_second_choice.classList.add("blue");
-  
-  
+
+
 footer.appendChild(button_first_choice);
 footer.appendChild(button_second_choice);
 
