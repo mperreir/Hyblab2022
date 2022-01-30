1ffef6fea441379b01901acb12875cbd67176005
Armand Lego <armand.lego@etu.univ-nantes.fr>
2022-01-30 14:20:52 +0100

    added 6th section

--- pouvoirs/public/js/views/p6.js
+++ pouvoirs/public/js/views/p6.js
@@ -4,9 +4,9 @@ const init_p6 = function() {
     const dialog_not_pressed = document.querySelector("#p6 .dialog-not-pressed");
     const decompte = document.querySelector("#p6 .decompte")
     const bouton_rouge = document.querySelector("#p6 .bouton-rouge");
-    const power_header = document.querySelector("#p6 .pouvoir-header")
+    const power_header = document.querySelector("#p6 .pouvoir-header");
+    const illu_fin = document.querySelector("#p6 .illu-fin");
 
-    dialog_before.style.display = "block";
     let timeout;
     let choice_made = 0;
 
@@ -20,13 +20,13 @@ const init_p6 = function() {
             complete : () => {
                 dialog_before.style.display = "none";
                 bouton_rouge.style.display = "block";
-                decompte.style.display = "block";
+                decompte.style.display = "flex";
                 anime({
                     targets : [decompte, bouton_rouge],
                     opacity : [0,1],
-                    easing : 'linear'
+                    easing : 'linear',
+                    complete : () => {print_decompte(5);}
                 });
-                timeout = setTimeout(decompte_func, 5000);
             }
         })
     });
@@ -42,16 +42,19 @@ const init_p6 = function() {
             duration : 150,
             complete : () => {
                anime({
-                   targets : decompte,
+                   targets : [decompte, bouton_rouge],
                    opacity : [1, 0],
                    easing : 'linear',
                    complete : () => {
                        decompte.style.display = "none";
-                       dialog_pressed.style.display = "block"
+                       bouton_rouge.style.display = "none";
+                       dialog_pressed.style.display = "block";
+                       illu_fin.style.display = "block";
+
                        anime({
-                            targets : dialog_pressed,
+                            targets : [dialog_pressed, illu_fin],
                             opacity : [0, 1],
-                            easing : 'linear'
+                            easing : 'linear',
                        })
                    }
 
@@ -71,9 +74,9 @@ const init_p6 = function() {
                        decompte.style.display = "none";
                        bouton_rouge.style.display = "none";
                        dialog_not_pressed.style.display = "block"
+                       illu_fin.style.display = "block";
                        anime({
-                            targets : dialog_not_pressed,
-                            translateY : "50%",
+                            targets : [dialog_not_pressed, illu_fin],
                             opacity : [0, 1],
                             easing : 'linear'
                        })
@@ -83,6 +86,21 @@ const init_p6 = function() {
 
 
     };
+
+    const print_decompte = function(i) {
+        decompte.querySelector("p").innerText = i;
+        anime({
+            targets : decompte,
+            opacity : [0.5, 1],
+            scale : [1.3, 1],
+            duration : 150,
+            easing : 'linear'
+        });
+        if(i > 0)
+            timeout = setTimeout(() => { print_decompte(i-1)}, 1000);
+        else
+            decompte_func();
+    }
 
 };
 
