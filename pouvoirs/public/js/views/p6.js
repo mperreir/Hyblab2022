3abdf823f1f61e895acbf8940dd858c7c36a939e
Armand Lego <armand.lego@etu.univ-nantes.fr>
2022-01-30 18:18:13 +0100

    fix on 6th section

--- pouvoirs/public/js/views/p6.js
+++ pouvoirs/public/js/views/p6.js
@@ -1,4 +1,4 @@
-const init_p6 = function() {
+const init_p6 = function () {
     const dialog_before = document.querySelector("#p6 .dialog-before");
     const dialog_pressed = document.querySelector("#p6 .dialog-pressed");
     const dialog_not_pressed = document.querySelector("#p6 .dialog-not-pressed");
@@ -6,13 +6,13 @@ const init_p6 = function() {
     const bouton_rouge = document.querySelector("#p6 .bouton-rouge");
     const power_header = document.querySelector("#p6 .pouvoir-header");
     const illu_fin = document.querySelector("#p6 .illu-fin");
-    const dialog_arrow = document.querySelector("#p6 .arrow")
+    const dialog_arrow = document.querySelector("#p6 .arrow");
 
     let timeout;
     let choice_made = 0;
 
     anime({
-        targets : dialog_arrow,
+        targets: dialog_arrow,
         translateY: "20%",
         direction: 'alternate',
         loop: true,
@@ -22,20 +22,20 @@ const init_p6 = function() {
 
     shakeElement(dialog_before);
 
-    dialog_before.addEventListener('click', () =>{
+    dialog_before.addEventListener('click', () => {
         anime({
-            targets : dialog_before,
+            targets: dialog_before,
             opacity: [1, 0],
             easing: 'linear',
-            complete : () => {
+            complete: () => {
                 dialog_before.style.display = "none";
                 bouton_rouge.style.display = "block";
                 decompte.style.display = "flex";
                 anime({
-                    targets : [decompte, bouton_rouge],
-                    opacity : [0,1],
-                    easing : 'linear',
-                    complete : () => {print_decompte(5);}
+                    targets: [decompte, bouton_rouge],
+                    opacity: [0, 1],
+                    easing: 'linear',
+                    complete: () => { print_decompte(5); }
                 });
             }
         })
@@ -43,73 +43,76 @@ const init_p6 = function() {
 
     bouton_rouge.addEventListener('click', () => {
         clearTimeout(timeout);
-        if(choice_made == 0){
-        anime({
-            targets : bouton_rouge,
-            scale : 0.98,
-            easing : 'linear',
-            direction : 'alternate',
-            duration : 150,
-            complete : () => {
-               anime({
-                   targets : [decompte, bouton_rouge],
-                   opacity : [1, 0],
-                   easing : 'linear',
-                   complete : () => {
-                       decompte.style.display = "none";
-                       bouton_rouge.style.display = "none";
-                       dialog_pressed.style.display = "block";
-                       illu_fin.style.display = "block";
+        if (choice_made == 0) {
+            choice_made = 1;
+            anime({
+                targets: bouton_rouge,
+                scale: 0.98,
+                easing: 'linear',
+                direction: 'alternate',
+                duration: 150,
+                complete: () => {
+                    anime({
+                        targets: [decompte, bouton_rouge],
+                        opacity: [1, 0],
+                        easing: 'linear',
+                        complete: () => {
+                            decompte.style.display = "none";
+                            bouton_rouge.style.display = "none";
+                            dialog_pressed.style.display = "block";
+                            illu_fin.style.display = "block";
 
-                       anime({
-                            targets : [dialog_pressed, illu_fin],
-                            opacity : [0, 1],
-                            easing : 'linear',
-                       })
-                   }
+                            anime({
+                                targets: [dialog_pressed, illu_fin],
+                                opacity: [0, 1],
+                                easing: 'linear',
+                            })
+                        }
 
-               }) 
-            }
-        });
-        choice_made = 1;
-    }
+                    })
+                }
+            });
+            choice_made = 1;
+        }
     });
-    
-    const decompte_func = function() {
-               anime({
-                   targets : [decompte, bouton_rouge],
-                   opacity : [1, 0],
-                   easing : 'linear',
-                   complete : () => {
-                       decompte.style.display = "none";
-                       bouton_rouge.style.display = "none";
-                       dialog_not_pressed.style.display = "block"
-                       illu_fin.style.display = "block";
-                       anime({
-                            targets : [dialog_not_pressed, illu_fin],
-                            opacity : [0, 1],
-                            easing : 'linear'
-                       })
-                   }
 
-               });
+    const decompte_func = function () {
+        if (choice_made == 0) {
+            choice_made = 1;
+            anime({
+                targets: [decompte, bouton_rouge],
+                opacity: [1, 0],
+                easing: 'linear',
+                complete: () => {
+                    decompte.style.display = "none";
+                    bouton_rouge.style.display = "none";
+                    dialog_not_pressed.style.display = "block"
+                    illu_fin.style.display = "block";
+                    anime({
+                        targets: [dialog_not_pressed, illu_fin],
+                        opacity: [0, 1],
+                        easing: 'linear'
+                    })
+                }
 
-
+            });
+        }
     };
 
-    const print_decompte = function(i) {
-        decompte.querySelector("p b").innerText = i;
-        anime({
-            targets : decompte,
-            opacity : [0.5, 1],
-            scale : [1.3, 1],
-            duration : 150,
-            easing : 'linear'
-        });
-        if(i > 0)
-            timeout = setTimeout(() => { print_decompte(i-1)}, 1000);
+    const print_decompte = function (i) {
+        if (i > 0)
+            timeout = setTimeout(() => { print_decompte(i - 1) }, 1200);
         else
             decompte_func();
+        decompte.querySelector("p b").innerText = i;
+
+        anime({
+            targets: decompte,
+            opacity: [0.5, 1],
+            scale: [1.3, 1],
+            duration: 150,
+            easing: 'linear'
+        });
     }
 
 };
