a06a97502cf63dd3669782a146f27a25ed09a291
Thomas Clouet <thomas.clouet1@etu.univ-nantes.fr>
2022-01-30 16:18:04 +0100

    :zap: V1 autocomplete

--- communes-1/public/js/js_anjoute-choix-commune.js
+++ communes-1/public/js/js_anjoute-choix-commune.js
@@ -2,3 +2,37 @@
 
 var viewport = document.querySelector('meta[name=viewport]');
 viewport.setAttribute('content', viewport.content + ', height=' + window.innerHeight);
+
+// Autocomplete
+const countries = [
+	{name: 'USA'},
+	{name: 'USA'},
+	{name: 'USA'},
+	{name: 'USA'},
+	{name: 'USA'},
+	{name: 'USA'},
+	{name: 'USA'},
+	{name: 'USA'},
+	{name: 'India'},
+	{name: 'Argentina'},
+	{name: 'Armenia'}
+  ];
+  
+const searchInput = document.querySelector('.input-communes');
+const suggestionsPanel = document.querySelector('.suggestions');
+
+searchInput.addEventListener('keyup', function() {
+const input = searchInput.value;
+suggestionsPanel.innerHTML = '';
+const suggestions = countries.filter(function(country) {
+	return country.name.toLowerCase().startsWith(input);
+});
+suggestions.forEach(function(suggested) {
+	const div = document.createElement('div');
+	div.innerHTML = suggested.name;
+	suggestionsPanel.appendChild(div);
+});
+if (input === '') {
+	suggestionsPanel.innerHTML = '';  
+}
+});
