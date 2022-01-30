3982655d8305f2edb6993e767e964649398ed3d1
Alexis VIE <alexis.vie@etu.univ-nantes.fr>
2022-01-30 18:39:08 +0100

    adding headphone img on first page and animation to rules page

--- pouvoirs/public/js/views/falling.js
+++ pouvoirs/public/js/views/falling.js
@@ -2,9 +2,11 @@
 
 const falling_slide = function(){
   const playButton = document.querySelector('#next-button');
+  const rules = createAnimation("background-falling","data/animations/rules.json",true);
   // Ligne à corriger avec l'unification de ce qu'on a fait
   playButton.addEventListener("click", event => {
     swiper.slideTo(2);
   });
+  rules.play();
 };
 
