d69f5704809c67b665ad5aa4db65512666bdaff4
Alexis VIE <alexis.vie@etu.univ-nantes.fr>
2022-01-31 18:54:21 +0100

    adding falling animation

--- pouvoirs/public/js/views/falling.js
+++ pouvoirs/public/js/views/falling.js
@@ -1,15 +1,66 @@
 "use strict";
-
+const fallingEffect = createAudio("data/sounds/fallingEffect.mp3",false,0.9,1.0);
+const oups = createAudio("data/sounds/oups.mp3",false);
+const swoosh = createAudio("data/sounds/swoosh.mp3",false);
+const background1 = createAnimation("background-container","data/animations/rules_background.json",true);
+const mouaFalling = createAnimation("moua-falling","data/animations/rules_moua.json",true);
 const falling_slide = function(){
-  const playButton = document.querySelector('#next-button');
-  const fallingEffect = createAudio("data/sounds/fallingEffect.mp3");
-  const rules = createAnimation("background-falling","data/animations/rules.json",true);
-  rules.play();
-  // Ligne à corriger avec l'unification de ce qu'on a fait
-  playButton.addEventListener("click", event => {
-    //fallingEffect.play();
-    swiper.slideTo(2);
+  background1.play();
+  mouaFalling.play();
+  document.querySelector('#next-button').addEventListener("click", event => {
+    makeFall();
   });
-  
 };
+
+function makeFall() {
+  anime({
+    targets: "#next-button, .bulle",
+    opacity: ['1', '0'],
+    loop: false,
+    easing: 'easeInOutCubic',
+    complete: function() {
+      swoosh.play();
+       anime({
+        targets: "#moua-falling svg",
+        translateY: '1000px',
+        loop: false,
+        easing: 'easeInOutCubic',
+        complete: (anim) => {
+          anime({
+            targets: "#background-container",
+            opacity: ['1', '0'],
+            loop: false,
+            easing: 'easeInOutCubic',
+            complete:function(){
+              fallingEffect.play();
+              const background2 = createAnimation("background-container","data/animations/mouaFallingOnPresident.json",false);
+              background1.destroy();
+              background2.play();
+              background2.addEventListener('complete', () => {
+                oups.play();
+                setTimeout(() => {
+                  anime({
+                    targets: "#background-container",
+                    opacity: ['1', '0'],
+                    loop: false,
+                    easing: 'easeInOutCubic',
+                    complete: () => swiper.slideTo(2)
+                  })
+                },1000);
+              });
+              anime({
+                targets: "#background-container",
+                opacity: ['0', '1'],
+                loop: false,
+                easing: 'easeInOutCubic'
+              });
+            }
+          })
+        }
+      })
+    }
+  })
+}
+
+
 
