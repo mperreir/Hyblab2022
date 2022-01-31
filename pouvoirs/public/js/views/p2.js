7b2d39e522e028ac33def6e561cca63737c9d2cf
Alexis VIE <alexis.vie@etu.univ-nantes.fr>
2022-01-31 17:06:12 +0100

    adding sound effect for pen

--- pouvoirs/public/js/views/p2.js
+++ pouvoirs/public/js/views/p2.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 const button = document.getElementById("send-button");
 const bulle = document.getElementById("bulle-container");
 const drawing = document.getElementById("drawing-container");
+const penSound = createAudio("data/sounds/pen.mp3",true,1,0.5);
 
 let coord = { x: 0, y: 0 };
 let enableButton = false;
@@ -69,12 +70,13 @@ function reposition(event) {
 
 function start(event) {
 	document.addEventListener("mousemove", draw);
+	penSound.play();
 	reposition(event);
 }
 
 function stop() {
 	document.removeEventListener("mousemove", draw);
-
+	penSound.pause();
 	// Enabling the button
 	if (enableButton) {
 		button.disabled = false;
