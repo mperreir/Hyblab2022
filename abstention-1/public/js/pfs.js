257a59986b4852ed6092ac3294657a3ff9f772c3
Fatma Hmida <fattouma220@gmail.com>
2022-02-01 11:03:50 +0100

    pierre feuille ciseaux

---
+++ abstention-1/public/js/pfs.js
@@ -0,0 +1,29 @@
+const buttons = document.querySelectorAll(".box button");
+console.log(buttons);
+// const resultat = document.querySelector(".resultat");
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function() {
+    const joueur = buttons[i].innerHTML;
+    const robot = buttons[Math.floor(Math.random() * buttons.length)].innerHTML;
+    let resultat = "";
+    // resultat.innerHTML = joueur + "       " + robot;
+    if (joueur===robot) {
+      resultat = "Egalité";
+    }
+    else if ((joueur === "Pierre" && robot === "Ciseaux") || (joueur === "Ciseaux" && robot === "Feuilles") || (joueur === "Feuilles") && (robot === "Pierre")) {
+      resultat = "Gagné";
+    }
+    else {
+      resultat = "Perdu";
+    }
+  document.querySelector(".resultat").innerHTML = `
+  Vous : ${joueur} </br>
+  Arthur : ${robot} <br/>
+  Résultat : ${resultat}
+`  ;
+    
+  });
+  
+  
+}
