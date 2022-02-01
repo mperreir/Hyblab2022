65202de3b7fe58181b0c0831d262d3c4c05f57a0
E20B110K <leopold.cudilla@etu.univ-nantes.fr>
2022-02-01 21:16:31 +0100

    changed game container height

--- q-a-2/public/js/pages/Step3Page.js
+++ q-a-2/public/js/pages/Step3Page.js
@@ -34,7 +34,8 @@ class Step3Page extends Page {
                 <GamePage
                     step={step}
                     title='Vérifie les déclarations des candidats.'
-                    subtitle="Les candidats ont rempli leur déclaration du patrimoine et des intérêts mais tout n'a pas besoin d'être déclaré. Décoche ce qui n'est pas nécessaire."
+                    subtitle="Décoche ce qui n'est pas nécessaire dans la déclaration du patrimoine et des intérêts des candidat·e·s"
+                    //subtitle="Les candidats ont rempli leur déclaration du patrimoine et des intérêts mais tout n'a pas besoin d'être déclaré. Décoche ce qui n'est pas nécessaire."
                     buttonTitle='Valider'
                     returnToExplanations={() => this.returnToExplanations()}
                 >
