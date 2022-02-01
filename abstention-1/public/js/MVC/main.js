21a5647eff9f2272585a36ce373264231478d930
Aurelien B <61365539+Neltab@users.noreply.github.com>
2022-02-01 16:41:40 +0100

    Ajout premier dialogue a l'ecole

--- abstention-1/public/js/MVC/main.js
+++ abstention-1/public/js/MVC/main.js
@@ -13,13 +13,77 @@ function mod(a,b) {
 	return (( a % b ) + b ) % b;
 };
 
+function getDialogueTransition(etape){
+    return [
+        {
+            visibility: [0.1, 0.2],
+            keyframes: ANIMATIONS.animations.fade.in,
+            player: "#Dialogue",
+        },
+        {
+            visibility: [0.1, 0.3],
+            keyframes: ANIMATIONS.animations[etape].perso1.in,
+            player: "#personnage1",
+        },
+        {
+            visibility: [0.1, 0.3],
+            keyframes: ANIMATIONS.animations[etape].perso2.in,
+            player: "#personnage2",
+        },
+        {
+            visibility: [0.7, 0.9],
+            keyframes: ANIMATIONS.animations[etape].perso1.out,
+            player: "#personnage1",
+        },
+        {
+            visibility: [0.7, 0.9],
+            keyframes: ANIMATIONS.animations[etape].perso2.out,
+            player: "#personnage2",
+        },
+        {
+            visibility: [0.8, 0.9],
+            keyframes: ANIMATIONS.animations.fade.out,
+            player: "#Dialogue",
+        }
+    ]
+}
+
 
 function getAnimations() {
-    return ANIMATIONS.ordre.map((etape, i, etapes) => {
-        const prevEtape = etapes[mod(i - 1, etapes.length)];
-        const nextEtape = etapes[(i+1) % etapes.length];
+    const etapes = ANIMATIONS.ordre;
 
-        return {
+    const firstAnim = {
+        container: `.container.dialogue.${etapes[0]}`,
+        actions: [
+            ...getDialogueTransition(etapes[0]),
+            {
+                visibility: [0.9, 1],
+                keyframes: ANIMATIONS.animations.fade.in,
+                player: `.player.${etapes[0]}-vers-${etapes[1]}`,
+            },
+        ]
+    };
+
+    const lastAnim = {
+        container: `.container.dialogue.${etapes[etapes.length - 1]}`,
+        actions: [
+            {
+                visibility: [0, 0.1],
+                keyframes: ANIMATIONS.animations.fade.out,
+                player: `.player.${etapes[etapes.length - 2]}-vers-${etapes[etapes.length - 1]}`,
+            },
+            ...getDialogueTransition(etapes[etapes.length - 1]),
+        ]
+    };
+
+    let anims = [];
+
+    for (let i = 1; i < etapes.length - 1; i++) {
+        const prevEtape = etapes[i - 1];
+        const etape = etapes[i];
+        const nextEtape = etapes[i + 1];
+
+        anims.push({
             container: `.container.dialogue.${etape}`,
             actions: [
                 {
@@ -63,9 +127,14 @@ function getAnimations() {
                     player: `.player.${etape}-vers-${nextEtape}`,
                 },
             ],
-        }
-    })
+        })
+
+    }
+
+
+    return [firstAnim, ...anims, lastAnim];
 }
+
 console.log(getAnimations());
 const animations = getAnimations().map(({container, actions}) => new AnimationModel(container, actions, dialogueControler));
 
@@ -75,9 +144,9 @@ scenes.forEach((scene, i) => scene.linkAnimationModel(animations[i]));
 
 
 LottieInteractivity.create({
-    player:'.player.ecole-vers-skatepark',
+    player:'.player.ecole1-vers-foraine',
     mode:"scroll",
-    container:".container.ecole-vers-skatepark",
+    container:".container.ecole1-vers-foraine",
     actions: [
         {
             visibility:[0,1],
@@ -88,9 +157,9 @@ LottieInteractivity.create({
 });
 
 LottieInteractivity.create({
-    player:'.player.skatepark-vers-camion',
+    player:'.player.foraine-vers-camion',
     mode:"scroll",
-    container:".container.skatepark-vers-camion",
+    container:".container.foraine-vers-camion",
     actions: [
         {
             visibility:[0,1],
@@ -127,9 +196,9 @@ LottieInteractivity.create({
 });
 
 LottieInteractivity.create({
-    player:'.player.mairie-vers-ecole',
+    player:'.player.mairie-vers-ecole2',
     mode:"scroll",
-    container:".container.mairie-vers-ecole",
+    container:".container.mairie-vers-ecole2",
     actions: [
         {
             visibility:[0,1],
