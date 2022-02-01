85c1de5253152426ab060d513da1a362bba02279
RomainG <romain.gentilhomme@etu.univ-nantes.fr>
2022-02-01 20:21:54 +0100

    add some animations

--- q-a-1/public/js/sommaire.js
+++ q-a-1/public/js/sommaire.js
@@ -1,30 +1,45 @@
 "use strict";
 
-// Init of the (touch friendly) Swiper slider
-/*const swiper = new Swiper("#mySwiper", {
-  direction: "vertical",
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});*/
+let bleu = document.getElementById("enveloppebleue");
+let rouge = document.getElementById("envelopperouge");
+let blanche = document.getElementById("enveloppeblanche");
+let carte = document.getElementById("carte");
 
+function reverse(){
+  bleu.style["-webkit-animation"] = 'enveloppebleue_reverse 1.5s';
+  bleu.style.animation = 'enveloppebleue_reverse 1.5s';
+  rouge.style["-webkit-animation"] = 'envelopperouge_reverse 1.5s';
+  rouge.style.animation = 'envelopperouge_reverse 1.5s';
+  blanche.style["-webkit-animation"] = 'enveloppeblanche_reverse 1.5s';
+  blanche.style.animation = 'enveloppeblanche_reverse 1.5s';
+  carte.style["-webkit-animation"] = 'carte_reverse 1.5s';
+  carte.style.animation = 'carte_reverse 1.5s';
+}
 
-// Wait for the video to preload and display 1st slide
-//const video = videojs(document.querySelector('#background-video'));
- 
-  // fade out the loader "slide"
-  // and send it to the back (z-index = -1)
-  anime({
-    delay: 1000,
-    targets: '#loader',
-    opacity: '0',
-    'z-index' : -1,
-    easing: 'easeOutQuad',
-  });
-  // Init first slide
-  initSlide1();
-  // Debug trace because the loadeddata event is
-  // sometime not fired
-  console.log("Video loaded");
+bleu.addEventListener("click", () => {
+  reverse();
+  setTimeout(() => { 
+    window.location.replace("informer.html");
+    }, 1000);
+})
 
+rouge.addEventListener("click", () => {
+  reverse();
+  setTimeout(() => { 
+    window.location.replace("procuration_traditional.html");
+    }, 1000);
+})
+
+blanche.addEventListener("click", () => {
+  reverse();
+  setTimeout(() => { 
+    window.location.replace("vote_traditional.html");
+    }, 1000);
+})
+
+carte.addEventListener("click", () => {
+  reverse();
+  setTimeout(() => { 
+    window.location.replace("check.html");
+    }, 1000);
+})
