445a6519f55a22be3d0f90ac656e225b1528989f
Malo Kervestin <malo.kervestin@etu.univ-nantes.fr>
2022-02-01 16:02:57 +0100

    scrollto

--- twitter-2/public/js/visu_donnees.js
+++ twitter-2/public/js/visu_donnees.js
@@ -4,28 +4,22 @@ function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// set the dimensions and margins of the graph
-const margin = { top: 0, right: 0, bottom: 0, left: 0 },
-    width = 400 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
-
 window.onload = () => {
+    window.scrollTo(0, 25);
+
     document.getElementById("return").addEventListener("click", () => {
         window.location.href = "./index.html";
     });
 
     const candidate = sessionStorage.getItem("selected_candidate");
     const theme = sessionStorage.getItem("selected_theme");
-    console.log(candidate)
 
     // append the svg object to the body of the page
     const svg = d3.select("#dataviz")
         .append("svg")
         .attr("width", "100%")
-        .attr("height", height)
+        .attr("height", 400)
         .append("g")
-    // .attr("transform",
-    //     `translate(${width / 2}, ${height / 2})`);
 
     fetch(`api/ratioNearCandidate/${theme}/${candidate}`)
         .then(res => res.json())
@@ -56,6 +50,7 @@ window.onload = () => {
                 });
 
             const ratio = node.append("text")
+                .attr("class", "lato")
                 .text(d => {
                     if (theme != "followers") {
                         return "" + parseFloat(d.ratio).toFixed(2) + "%";
@@ -71,7 +66,7 @@ window.onload = () => {
                     .links(data.links)                                    // and this the list of links
                 )
                 .force("charge", d3.forceManyBody().strength(function () {
-                    return random(-2500, -3000);
+                    return random(-3000, -3500);
                 }))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
                 .force("center", d3.forceCenter(150, 150))     // This force attracts nodes to the center of the svg area
                 .on("end", ticked);
@@ -82,7 +77,7 @@ window.onload = () => {
                     .attr('transform', d => `translate(${d.x}, ${d.y})`)
 
                 shortname
-                    .attr('dy', d => 50 + parseFloat(d.ratio) + 10)
+                    .attr('dy', d => 50 + parseFloat(d.ratio) + 12)
                     .attr('transform', d => `translate(${d.x}, ${d.y})`);
 
                 ratio
