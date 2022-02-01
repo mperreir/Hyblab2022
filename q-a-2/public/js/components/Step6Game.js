1f0be3a97b56ebfd71aa29e2e2c6d20a29550c42
Simon Perrin <prrn.simon@gmail.com>
2022-02-01 10:56:12 +0100

    GamePage component

---
+++ q-a-2/public/js/components/Step6Game.js
@@ -0,0 +1,13 @@
+class Step6Game extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className='step6Game'>
+                
+            </div>
+        )
+    }
+}
