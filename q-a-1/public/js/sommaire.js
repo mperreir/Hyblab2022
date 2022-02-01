0ecb6c40f0269d87a23852372d9de226d077423b
RomainG <romain.gentilhomme@etu.univ-nantes.fr>
2022-02-01 20:55:06 +0100

    add return and credit buttons with animations on sommaire page

--- q-a-1/public/js/sommaire.js
+++ q-a-1/public/js/sommaire.js
@@ -5,6 +5,9 @@ let rouge = document.getElementById("envelopperouge");
 let blanche = document.getElementById("enveloppeblanche");
 let carte = document.getElementById("carte");
 
+let retour = document.getElementById("return");
+let credits = document.getElementById("credits")
+
 function reverse(){
   bleu.style["-webkit-animation"] = 'enveloppebleue_reverse 1.5s';
   bleu.style.animation = 'enveloppebleue_reverse 1.5s';
@@ -14,6 +17,10 @@ function reverse(){
   blanche.style.animation = 'enveloppeblanche_reverse 1.5s';
   carte.style["-webkit-animation"] = 'carte_reverse 1.5s';
   carte.style.animation = 'carte_reverse 1.5s';
+  retour.style["-webkit-animation"] = 'slide-out-bck-center 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both';
+  retour.style.animation = 'slide-out-bck-center 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both';
+  credits.style["-webkit-animation"] = 'slide-out-bck-center 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both';
+  credits.style.animation = 'slide-out-bck-center 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both';
 }
 
 bleu.addEventListener("click", () => {
@@ -41,5 +48,19 @@ carte.addEventListener("click", () => {
   reverse();
   setTimeout(() => { 
     window.location.replace("check.html");
+    }, 1000);
+})
+
+retour.addEventListener("click", () => {
+  reverse();
+  setTimeout(() => { 
+    window.location.replace("index.html");
+    }, 1000);
+})
+
+credits.addEventListener("click", () => {
+  reverse();
+  setTimeout(() => { 
+    window.location.replace("credit.html");
     }, 1000);
 })
