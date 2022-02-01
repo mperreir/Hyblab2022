b35ccbc13ee0e8214e21c285e1101c834b949027
Thomas Clouet <thomas.clouet1@etu.univ-nantes.fr>
2022-02-01 20:34:17 +0100

    :art: Nettoyage

--- communes-1/public/js/js_anjoute-game-1.js
+++ communes-1/public/js/js_anjoute-game-1.js
@@ -36,28 +36,18 @@ function toLowerMdr(nom)
 	}
 	return temp
 }
-const logo_joute = document.querySelector('#logo-joute');
-logo_joute.classList.add('animate__animated', 'animate__fadeInDown');
 
-logo_joute.addEventListener('animationend', () => {
-	logo_joute.classList.add('animate__animated', 'animate__fadeOutDown');
-
-	logo_joute.addEventListener('animationend', () => {
-		loader.textContent = '';
-	});
-
-	const commune_enemy = document.querySelector('.commune')
-	const commune_enemy_deux = document.querySelector('.commune_2')
-	const question = document.querySelector('.question')
-	const reponse_1 = document.querySelector('.reponse_1')
-	const reponse_2 = document.querySelector('.reponse_2')
-	const enemy = generate_enemy("55");
-	const factory = new FactoryQuestion();
-	console.log(enemy)
-	const question_data = factory.generate_question(1,true,enemy[0][0]);
-	question.textContent = question_data[0];
-	reponse_1.firstChild.textContent = toLowerMdr(question_data[1][0]);
-	reponse_2.firstChild.textContent = toLowerMdr(question_data[2][0]);
-	commune_enemy.textContent = enemy[0][1];
-	commune_enemy_deux.textContent = enemy[0][1]
-});
+const commune_enemy = document.querySelector('.commune')
+const commune_enemy_deux = document.querySelector('.commune_2')
+const question = document.querySelector('.question')
+const reponse_1 = document.querySelector('.reponse_1')
+const reponse_2 = document.querySelector('.reponse_2')
+const enemy = generate_enemy("55");
+const factory = new FactoryQuestion();
+console.log(enemy)
+const question_data = factory.generate_question(1,true,enemy[0][0]);
+question.textContent = question_data[0];
+reponse_1.firstChild.textContent = toLowerMdr(question_data[1][0]);
+reponse_2.firstChild.textContent = toLowerMdr(question_data[2][0]);
+commune_enemy.textContent = enemy[0][1];
+commune_enemy_deux.textContent = enemy[0][1]
