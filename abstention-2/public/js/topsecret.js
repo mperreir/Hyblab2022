55233bc293b932389a25637bcf5ac9a5de30c9d4
axel <axel.rocheteau4@gmail.com>
2022-02-01 14:35:57 +0100

    architecture

---
+++ abstention-2/public/js/topsecret.js
@@ -0,0 +1,6 @@
+async function loadTopSecret() {
+  const container = document.getElementById('container');
+  const data = { ville: "Saint-herblain", pop: 46268, superficie: 30.02, metier:[ 0.04, 2.07, 9.05, 13.03, 14.11, 8.82, 21.27, 12.55]};
+  const fileExplorerHtml = await loadTemplate('templates/topsecret.ejs', data);
+  container.innerHTML = fileExplorerHtml;
+};
