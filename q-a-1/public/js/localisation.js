10252938ed9e68ff506367e67b2abf66d6654748
Florian Dargère <52320926+floriandrg@users.noreply.github.com>
2022-02-01 10:10:58 +0100

    test popup

--- q-a-1/public/js/localisation.js
+++ q-a-1/public/js/localisation.js
@@ -88,7 +88,12 @@ function geoFindMe() {
   
   }
 
-  
+function ouvre_popup(page) {
+  window.open(page,"nom_popup","menubar=no, status=no, scrollbars=no, menubar=no, width=200, height=100");
+}
+
+ouvre_popup('/test');
+
 document.addEventListener('DOMContentLoaded', geoFindMe);
 document.console.log(mapLink.textContent);
 
