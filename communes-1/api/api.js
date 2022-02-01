e9ae2d1aa9dbe83fb371f20823a55bc586596154
Thomas Clouet <thomas.clouet1@etu.univ-nantes.fr>
2022-02-01 11:02:19 +0100

    :zap: données serveur

--- communes-1/api/api.js
+++ communes-1/api/api.js
@@ -1,7 +1,8 @@
 'use strict';
 
-const app = require( 'express' )();
+const app = require('express')();
 const path = require('path');
+const chargemenet = require('./../dataInGame/chargement.js');
 
 // Sample endpoint that sends the partner's name
 app.get('/topic', function ( req, res ) {
@@ -11,7 +12,11 @@ app.get('/topic', function ( req, res ) {
     topic = path.basename(path.join(__dirname, '/..'))
     // Send it as a JSON object
     res.json({'topic':topic});
-} );
+});
+
+app.get('/dataElection', (req, res) => {
+	// générer une question et sa réponse
+});
 
 // Export our API
 module.exports = app;
