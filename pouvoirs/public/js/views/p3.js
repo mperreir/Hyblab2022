95cdccd1a324c6eb87b2f149bfcd5b0fcb586b40
E20A805D <timon.deschamps@etu.univ-nantes.fr>
2022-02-01 11:41:48 +0100

    relative page + timeout + fixes

--- pouvoirs/public/js/views/p3.js
+++ pouvoirs/public/js/views/p3.js
@@ -17,9 +17,6 @@ const init_p3 = function() {
   let wooshWasPlayed = false;
 
   const slide = document.getElementById("p3");
-  slide.addEventListener('click', () => {
-    animateLoading();
-  })
 
   // info
   let state = 0;
@@ -37,23 +34,21 @@ const init_p3 = function() {
   })
   const img = document.createElement("img");
   img.setAttribute("src", "img/elements/illu_p3.svg")
-  img.setAttribute("height", "300px");
+  img.style.height = "40%";
 
 
   let illustration = {
-    illu: '300px',
+    illu: '40%',
     infos: '85%'
   }
 
   function animateLoading() {
     anime({
       targets: illustration,
-      illu: '150px',
       infos: '55%',
       loop: false,
       easing: 'easeInOutCubic',
       update: function(anim) {
-        img.style.height = illustration.illu;
         infos.style.height = illustration.infos;
       }
     })
@@ -65,15 +60,14 @@ const init_p3 = function() {
   };
 
 
-  const metabox = document.createElement("div");
-  metabox.style.height = "auto";
+  setTimeout(animateLoading, 2000);
+  slide.addEventListener('click', animateLoading);
 
+  const metabox = document.createElement("div");
 
   const diBox = document.createElement("div");
   metabox.appendChild(diBox);
   diBox.style.position = "relative";
-  diBox.style.verticalAlign = "top";
-  diBox.style.height = "125px";
 
   diBox.style.width = "100%";
   diBox.style.margin = "0px auto";
@@ -112,35 +106,34 @@ const init_p3 = function() {
   const invalid = document.createElement('img');
   invalid.src = "img/elements/button_invalid.svg";
   [valid, invalid].forEach(x => {
-    x.style.height = "20px";
+    x.style.height = "50%";
     x.style.position = "absolute";
     x.style.left = "-50%";
-    x.style.top = "15px"
+    x.style.top = "25%"
   })
 
 
   const buttons = document.createElement("div");
   buttons.classList.add("choices");
-  buttons.style.height = "30%";
+  buttons.style.height = "25%";
   var x1 = document.createElement("BUTTON");
   x1.innerHTML = "Je serai plus efficace en dirigeant seul.";
   var x2 = document.createElement("BUTTON");
   x2.innerHTML = "Je peux nommer un responsable.";
   var x3 = document.createElement("BUTTON");
-  x3.innerHTML = "C'est mon gouvernement ! Je nomme tous ceux qui y siègent !"
+  x3.innerHTML = "C'est mon gouvernement !<br>Je nomme tous ceux qui y siègent !"
 
   const t1 = "Mauvaise réponse ! La Vème république est une démocratie, et le Président ne peut pas..."
   const t2 = "Bonne réponse ! Le président nomme un Premier Ministre qui lui propose ensuite des mi...."
   const t3 = "Mauvaise réponse ! Il faut passer par un intermédiaire qui propose au président des ministres..."
 
-  const colors = ["red", "#1be5b9", "red"];
+  const colors = ["#FF2019", "#1be5b9", "#FF2019"];
   function changeText(nb, txt, btn) {
     if (state === nb){
       swiper.disable();
       cancel.play();
       textContainer.innerHTML = originalText;
       btn.querySelector("img").remove();
-      // shakeElement(diBox);
       btn.style.backgroundColor = "#D4C2F0";
       state = 0;
     }
@@ -178,15 +171,20 @@ const init_p3 = function() {
 
   css([x1, x2, x3], {
     'background-color' : '#D4C2F0',
+    'font-size' : '15%',
     'color' : 'white',
     'width' : '80%',
-    'height' : '50px',
-    'padding': '10px 20px',
-    'margin': '2% auto',
+    'height' : '25%',
+    'padding': '0px 20px',
+    'margin': '2.5% auto',
     'display' : 'block',
     'border': 'none',
     'border-radius': '12px',
-    'position' : 'relative'
+    'position' : 'relative',
+    'box-sizing': 'border-box',
+    '-moz-box-sizing': 'border-box',
+    '-webkit-box-sizing': 'border-box',
+    'vertical-align' : 'middle'
   })
 
 
