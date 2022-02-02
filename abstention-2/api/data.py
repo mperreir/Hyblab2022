6d8bd4078fc529c268ebb8d9a2bd7ccbdf81faf7
Victorien Plot <victorien.plot@etu.univ-nantes.fr>
2022-02-02 20:38:24 +0100

    Fix autocomplete

--- abstention-2/api/data.py
+++ abstention-2/api/data.py
@@ -4,5 +4,5 @@ import json
 with open('data.json', 'r', encoding='utf-8') as json_file:
     data = json.load(json_file)
     cities_map = {k: data[k]["nom"] for k in data.keys()}
-    with open('cities_map.json', 'w') as cities_file: 
-        json.dump(cities_map, cities_file)
+    with open('cities_map.json', 'w', encoding='utf-8') as cities_file: 
+        json.dump(cities_map, cities_file, ensure_ascii=False)
