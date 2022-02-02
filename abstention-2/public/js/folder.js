82b374c6acd0e6d330bd6ca5ffdf98035081fcf6
axel <axel.rocheteau4@gmail.com>
2022-02-02 21:49:34 +0100

    graph2.0

--- abstention-2/public/js/folder.js
+++ abstention-2/public/js/folder.js
@@ -222,7 +222,7 @@ async function loadFolder(folderName) {
 function getEtrangersData(data) {
     renderData = [];
     renderColor = [];
-    for (i = 0; i < data[0]; i++) {
+    for (i = 0; i < Math.floor(data[0]); i++) {
         d = {
             x: i % 3,
             y: Math.floor(i / 3),
@@ -232,13 +232,13 @@ function getEtrangersData(data) {
         renderColor.push('rgb(0, 57, 255)');
     }
     renderData.push({
-        x: Math.floor((data[0]+1) % 3),
-        y: Math.floor((data[0]+1) / 3),
+        x: Math.floor((data[0]) % 3),
+        y: Math.floor((data[0]) / 3),
         r: Math.floor((data[0] - Math.floor(data[0])) * 10)
     })
     renderColor.push('rgb(0, 57, 255)');
 
-    for (i = 0; i < data[1]; i++) {
+    for (i = 0; i < Math.floor(data[1]); i++) {
         d = {
             x: i % 3 + 4,
             y: Math.floor(i / 3),
@@ -248,8 +248,8 @@ function getEtrangersData(data) {
         renderColor.push('rgb(255, 72, 0)');
     }
     renderData.push({
-        x: Math.floor((data[1]+1) % 3) + 4,
-        y: Math.floor((data[1]+1) / 3),
+        x: Math.floor((data[1]) % 3) + 4,
+        y: Math.floor((data[1]) / 3),
         r: Math.floor((data[1] - Math.floor(data[1])) * 10)
     });
     renderColor.push('rgb(255, 72, 0)');
