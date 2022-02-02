c3e56027b4a0f84031346e5a9f1dd0a34e8ac5b1
baptiste musset <john.doe@etu.univ-nantes.fr>
2022-02-02 23:00:24 +0100

    animations ronds

--- q-a-1/public/js/draw.js
+++ q-a-1/public/js/draw.js
@@ -1,6 +1,9 @@
 
 const activateur = document.getElementById("activateur");
+const activateur2 = document.getElementById("activateur2")
+
 const svg=document.getElementById("svg1");
+const svg2=document.getElementById("svg2");
 
 activateur.addEventListener('click',function(){
     
@@ -13,3 +16,16 @@ svg.addEventListener('click',function(){
     
     svg.classList.add("dessin_rond");
 })
+
+activateur2.addEventListener('click',function(){
+    
+    console.log("test");
+    svg2.classList.add("dessin_rond");
+})
+
+svg2.addEventListener('click',function(){
+    
+    
+    svg2.classList.add("dessin_rond");
+})
+
