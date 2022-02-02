f889b92f83d8e4eb864929f01fbfe0c4b994b85a
Julie j <julie@LAPTOP-0IJTLB1K>
2022-02-02 01:09:10 +0100

    Add 2nd game

--- q-a-2/public/js/components/Step2Game.js
+++ q-a-2/public/js/components/Step2Game.js
@@ -3,70 +3,69 @@ class Step2Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            candidates: [],
-            hint: false,
-        }
+            selected: Array(6).fill(false),
+            IDloser:Math.floor(Math.random()*6),
+        };
+
+        candidates[this.state.IDloser].stepTwoGame.valid = false;
     }
 
-    check(i) {
-        if(this.state.checked.includes(i)) {
-            document.getElementById('step2Game-character-' + i).setAttribute('class', 'step2Game-checkbox step2Game-checkbox-unchecked');
-            this.state.checked = this.state.checked.filter(e => e !== i);
-        }
-        else {
-            console.log(document.getElementById('step2Game-character-' + i));
-            document.getElementById('step2Game-character-' + i).setAttribute('class', 'step2Game-checkbox step2Game-checkbox-checked');
-            console.log(document.getElementById('step2Game-character-' + i));
-            this.state.checked.push(i);
-            console.log(this.state.checked);
-        }
+    handleClick(i) {
+        const tmpcpy = this.state.selected.slice();
+        tmpcpy[i] = !tmpcpy[i]
+        this.setState({selected: tmpcpy});
+    }
+
+    checkWin() {
+        let win = true;
+        if (this.state.selected[this.state.IDloser]) win = false;
+
+        this.state.selected.forEach((e, i) => {
+            if(i !== this.state.IDloser && e === false) win = false;
+        });
+
+        return win;
     }
 
     render() {
-        this.state.candidates = [0,1,2,3,4,5];
-        
-        console.log(candidates);
 
-        const imageCandidates = this.state.candidates.map(i => (
-            <svg width="100%" height="100%">
-                <image href={'img/step2Game/signature-perso-' + i + '.svg'}></image>
-                <g>
-                <text
-                    className='step2Game_signature_text'
-                    x={candidates[i].stepTwoGame.x}
-                    y={candidates[i].stepTwoGame.y}
-                    transform={candidates[i].stepTwoGame.transform}
-                    fontSize={candidates[i].stepTwoGame.fontsize}>
-                {candidates[i].stepTwoGame.signatures}
-                </text>
-                </g>
-            </svg>));
-
-        // let  i = 0;
-        // const test = (
-        //     <svg width='200' height='200'>
-        //         <image href={'img/step2Game/signature-perso-' + i + '.svg'}></image>
-        //         <g transform='translate(0,0)'>
-        //         <text
-        //             className='step2Game_signature_text'
-        //             x={candidates[i].stepTwoGame.x}
-        //             y={candidates[i].stepTwoGame.y}
-        //             transform={candidates[i].stepTwoGame.transform}
-        //             fontSize={candidates[i].stepTwoGame.fontsize}>
-        //         {candidates[i].stepTwoGame.signatures}
-        //         </text>
-        //         </g>
-        //     </svg>
-        //     );
+        const imageCandidates = candidates.map((e, i) => {
+            return (
+            <th 
+                id={"step2Game_perso_" + i} 
+                className={this.state.selected[i] ? "step2Game_img_selected" : "step2Game_img_not_selected"}
+                onClick={() => this.handleClick(i)}
+                width="30%">
+                <svg className="step2Game_img" viewBox="0 0 100 100" transform={candidates[i].stepTwoGame.scale}>
+                    <g>
+                        <image 
+                            width="100%" 
+                            height="100%" 
+                            href={'img/step2Game/signature-perso-' + i + (candidates[i].stepTwoGame.valid ? '.svg' : '-F.svg')}
+                            >
+                        </image>
+                    </g>
+                </svg>
+            </th>)});
 
         return(            
             <div className='step2Game'>
                 <Header step={2} />
                 <h1 className='step2Game-title'>À toi de jouer ! Valide le nombre de signatures</h1>
-                <p className='step2Game-explaination'>Voici le nombre de signatures de chaque candidat. Sélectionne ceux qui peuvent continuer.</p>
-                <div className='step2Game-images-container'>
-                    <svg height='100%' width='100%' viewBox='0 0 900 600'>{imageCandidates}</svg>
-                </div>
+                <p className='step2Game-explaination'>Voici le nombre de signatures de chaque candidat·e. Sélectionne celleux qui peuvent continuer.</p>
+                <table className='step2Game-images-container'>
+                    <tr>
+                        {imageCandidates[0]}
+                        {imageCandidates[1]}
+                        {imageCandidates[2]}
+                    </tr>
+                    <tr>
+                        {imageCandidates[3]}
+                        {imageCandidates[4]}
+                        {imageCandidates[5]}
+                    </tr>
+                </table>
+                {/* TODO: Ajouter le passage à la page suivante */}
                 <Button value={'Valider'} disabled={false} white={false} onClick={() => this.props.buttonOnClick()}/>
             </div>
         );
