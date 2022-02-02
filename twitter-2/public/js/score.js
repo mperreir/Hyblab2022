937738768ecd932500a7223a35fec9715881817b
doumencp <pierrr.doumenc@gmail.com>
2022-02-02 15:28:09 +0100

    fix de placements

--- twitter-2/public/js/score.js
+++ twitter-2/public/js/score.js
@@ -7,12 +7,12 @@ window.onload = () => {
    sessionStorage.removeItem("score");
    if (score >= 2) {
       document.body.style.backgroundImage = "url('img/anim-confetti-quiz_1.gif')";
-      document.getElementById("fail").style.visibility = "hidden";
       document.getElementById("success").style.visibility = "visible";
       document.querySelector("video").style.visibility = "visible";
    }
    else{
       document.getElementById("sentence").appendChild(document.createElement("br"));
+      document.getElementById("fail").style.visibility = "visible";
    }
    document.getElementById("new-quiz").addEventListener("click", () => {
       window.location.href = "./question.html";
