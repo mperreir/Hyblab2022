3d6e7e12edc96f97e63438119312db91437c76c9
E20B110K <leopold.cudilla@etu.univ-nantes.fr>
2022-02-02 21:48:03 +0100

    removed testcomponent from index html

--- q-a-2/public/js/index.js
+++ q-a-2/public/js/index.js
@@ -9,7 +9,7 @@ candidates.forEach(candidate => {
   let step = Math.floor(Math.random() * (Math.floor(9) - Math.ceil(1)) + Math.ceil(1));
   stepsCandidates[step].push(candidate);
 })*/
-stepsCandidates["3"] = [...candidates];
+stepsCandidates["1"] = [...candidates];
 //stepsCandidates["6"] = candidates.slice(0, 5);
 //stepsCandidates["7"] = [];
 //stepsCandidates['8'].push(candidates[0]);
@@ -19,16 +19,16 @@ class App extends React.Component {
     super(props);
     this.state = {
       stepPages: [
-        /*<StartingPage nextStep={() => this.nextStep()} />,
-        <Step1Page data={data} nextStep={() => this.nextStep()} />,
-        <Step2Page data={data} nextStep={() => this.nextStep()} />,*/
+        //<StartingPage nextStep={() => this.nextStep()} />,
+        //<Step1Page data={data} nextStep={() => this.nextStep()} />,
+        <Step2Page data={data} nextStep={() => this.nextStep()} />,
         <Step3Page data={data} nextStep={() => this.nextStep()} />,
-        <Step4Page data={data} nextStep={() => this.nextStep()} />,
+        /*<Step4Page data={data} nextStep={() => this.nextStep()} />,
         <Step5Page data={data} nextStep={() => this.nextStep()} />,
         <Step6Page data={data} nextStep={() => this.nextStep()} />,
         <Step7Page data={data} nextStep={() => this.nextStep()} />,
         <Step8Page data={data} nextStep={() => this.nextStep()} />,
-        <EndingPage nextStep={() => this.resetStep()} />
+        <EndingPage nextStep={() => this.resetStep()} />*/
       ],
       step: 0
     }
@@ -49,6 +49,12 @@ class App extends React.Component {
     return this.state.stepPages[this.state.step];
   }
 }
+
+function randomizeCandidates() {
+
+}
+
+randomizeCandidates();
 
 ReactDOM.render(
   <App />,
