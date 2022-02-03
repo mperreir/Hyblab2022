af2a8900ba2d16a3a242da95458d84eeb627f924
Armand Lego <armand.lego@etu.univ-nantes.fr>
2022-02-03 16:31:42 +0100

    Added arrow + resize creative commons

--- pouvoirs/public/js/utils/arrow.js
+++ pouvoirs/public/js/utils/arrow.js
@@ -1,7 +1,8 @@
-const showArrow = function(){
+const showArrow = function () {
+    document.getElementById("arrowSwipe").addEventListener('click', () => wrapper_nextSlide(), { once: true });
     document.getElementById("arrowSwipe").style.visibility = "visible";
 }
 
-const hideArrow = function(){
+const hideArrow = function () {
     document.getElementById("arrowSwipe").style.visibility = "hidden";
 }
