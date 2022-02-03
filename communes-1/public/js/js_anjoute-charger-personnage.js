ba7f6c9733b5f0a51c8c60f56fe345884d1ef255
Thomas Clouet <thomas.clouet1@etu.univ-nantes.fr>
2022-02-03 11:36:15 +0100

    :bug: anim

--- communes-1/public/js/js_anjoute-charger-personnage.js
+++ communes-1/public/js/js_anjoute-charger-personnage.js
@@ -1,5 +1,5 @@
-const video = document.querySelector('.animation_perso');
-video.innerHTML = '';
+const bvideo = document.querySelector('.bloc-video');
+bvideo.innerHTML = '';
 
 let personnages = [
 	'img/animations/manteau_rouge.mp4',
@@ -14,8 +14,19 @@ let personnages = [
 	'img/animations/dame-reflet.mp4',
 ]
 
+let video = document.createElement('video');
+video.loop = true;
+video.autoplay = true;
+video.muted = true;
+video.playsInline = true;
+video.setAttribute('class', 'animation_perso');
+
+
 let source = document.createElement('source');
 source.setAttribute('src', personnages[localStorage.getItem('position_joueur')-1]);
 source.setAttribute('type', 'video/mp4');
 
+
 video.appendChild(source);
+
+bvideo.appendChild(video);
