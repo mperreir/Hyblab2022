8e4e97226f367325626013be1052b704892b0383
Thomas Clouet <thomas.clouet1@etu.univ-nantes.fr>
2022-02-03 08:59:13 +0100

    :zap: nb recompenses fin

--- communes-1/public/js/js_anjoute-victoire.js
+++ communes-1/public/js/js_anjoute-victoire.js
@@ -1,5 +1,15 @@
 const restart = document.querySelector('.restart');
+const label_joute = document.querySelector('.label-joute');
 
 restart.addEventListener('click', () => {
 	window.location.href = 'index.html';
 });
+
+
+let nb_objets = localStorage.getItem('inventaire').split(',').length - 1;
+let richesse = ' richesse';
+if (nb_objets > 1) {
+	richesse = ' richesses'
+}
+label_joute.innerHTML = 'Tu as amassé ' + nb_objets + richesse + ' sur un total de 10 !';
+
