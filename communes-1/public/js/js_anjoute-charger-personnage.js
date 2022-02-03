ab8b8e4303ea348a7965d0a79077097a7eac2be0
Gabriel JOLLY <gabriel-jolly@orange.fr>
2022-02-03 13:11:37 +0100

    test video.webm

--- communes-1/public/js/js_anjoute-charger-personnage.js
+++ communes-1/public/js/js_anjoute-charger-personnage.js
@@ -3,7 +3,7 @@ bvideo.innerHTML = '';
 
 let personnages = [
 	'img/animations/manteau_rouge.mp4',
-	'img/animations/manteau_bleu.mp4',
+	'img/animations/manteau_bleu.webm',
 	'img/animations/manteau_vert.mp4',
 	'img/animations/bonnie.mp4',
 	'img/animations/chapeau.mp4',
@@ -24,7 +24,7 @@ video.setAttribute('class', 'animation_perso');
 
 let source = document.createElement('source');
 source.setAttribute('src', personnages[localStorage.getItem('position_joueur')-1]);
-source.setAttribute('type', 'video/mp4');
+source.setAttribute('type', 'video/webm');
 
 
 video.appendChild(source);
