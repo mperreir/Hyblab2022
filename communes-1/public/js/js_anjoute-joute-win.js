8673ccb4f1eeec2892ba2a5e32cc4de9f5ea2ad6
Gabriel JOLLY <gabriel-jolly@orange.fr>
2022-02-03 17:14:53 +0100

    patch chargement image

--- communes-1/public/js/js_anjoute-joute-win.js
+++ communes-1/public/js/js_anjoute-joute-win.js
@@ -1,27 +1,41 @@
 const recompense_div = document.querySelector('.recompense');
 const label_joute = document.querySelector('.label-joute-recompense');
 
+function toLowerMdr(nom)
+{
+	let temp = "";
+	for (var i = 0; i < nom.length; i++) {
+		if(nom[i].toLowerCase() !=="l") {
+			temp += nom.charAt(i).toLowerCase();
+		}
+		else {
+			temp += nom.charAt(i).toUpperCase();
+		}
+	}
+	return temp
+}
+
 function bonneVersion(name){
 	switch (name){
-		case 'Un Magnum De Cointreau':
+		case 'Un magnum de Cointreau':
 			return "img/recompenses/Un_Magnum_De_Cointreau.png"
-		case 'BouLes De Fort Gagnantes':
+		case 'Boules de fort gagnantes':
 			return "img/recompenses/Boules_De_Fort_Gagnantes.png"
-		case 'L\'oie Aux Œufs D\'or':
+		case 'L\'oie aux Œufs d\'or':
 			return "img/recompenses/L_oie_Aux_Œufs_D_or.png"
-		case 'Une MédaiLLe Du Roi René':
+		case 'Une médaille du roi René':
 			return "img/recompenses/Médaille_Du_Roi_René.png"
-		case 'Une Soupe Angevine A VoLonté':
+		case 'Une soupe angevine à volonté':
 			return "img/recompenses/Soupe_Angevine_À_Volonté_.png"
-		case 'Les MeiLLeurs Bottereaux':
+		case 'Les meilleurs bottereaux':
 			return "img/recompenses/Les_Meilleurs_Bottereaux_.png"
-		case 'Le Manuscrit De Joachim Du BeLay':
+		case 'Le manuscrit de Joachim du Belay':
 			return "img/recompenses/Manuscrit_De_Joachim_Du_Belay_.png"
-		case'Le Chateau D \'Angers':
+		case 'Le château d \'Angers':
 			return "img/recompenses/Château_D_Angers.png"
-		case 'Un ChevaL Du Cadre Noir':
+		case 'Un cheval du Cadre noir':
 			return "img/recompenses/Cheval_Du_Cadre_Noir.png"
-		case 'Le Bateau De Loire Express':
+		case 'Le bateau de Loire Express':
 			return "img/recompenses/Bateau_De_Loire_Expres.png"
 	}
 
@@ -36,4 +50,4 @@ img.setAttribute('alt', objet);
 
 recompense_div.appendChild(img);
 
-label_joute.innerHTML = objet;
+label_joute.innerHTML = toLowerMdr(objet);
