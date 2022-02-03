1e503d0eff83ef37824d978c43f098e92ab6bfbb
baptiste musset <john.doe@etu.univ-nantes.fr>
2022-02-03 18:03:07 +0100

    fin croix

--- q-a-1/public/js/draw.js
+++ q-a-1/public/js/draw.js
@@ -5,8 +5,15 @@ const activateur2 = document.getElementById("activateur2")
 const svg=document.getElementById("svg1");
 const svg2=document.getElementById("svg2");
 
+const Croix1 =document.getElementById("Croix1");
+const Croix2 =document.getElementById("Croix2");
+
+const False1=document.getElementById("False1");
+const False2=document.getElementById("False2");
+
+
 activateur.addEventListener('click',function(){
-    
+
     
     svg.classList.add("dessin_rond");
 })
@@ -16,6 +23,30 @@ svg.addEventListener('click',function(){
     
     svg.classList.add("dessin_rond");
 })
+
+Croix1.addEventListener('click', function()
+{
+    console.log("test");
+    Croix1.classList.add("dessin_croix");
+    
+})
+
+False1.addEventListener("click", function()
+{
+    console.log("test");
+    Croix1.classList.add("dessin_croix");
+})
+
+Croix2.addEventListener('click', function()
+{
+    Croix2.classList.add("dessin_croix");
+});
+
+False2.addEventListener('click', function()
+{
+    Croix2.classList.add("dessin_croix");
+})
+
 
 activateur2.addEventListener('click',function(){
     
@@ -29,36 +60,21 @@ svg2.addEventListener('click',function(){
     svg2.classList.add("dessin_rond");
 })
 
-const Croix1 =document.getElementById("Croix1");
-const Croix2 =document.getElementById("Croix2");
+
 const Croix3 =document.getElementById("Croix3");
 const Croix4 =document.getElementById("Croix4");
 
-const False1=document.getElementById("False1");
-const False2=document.getElementById("False2");
+
+
 const False3=document.getElementById("False3");
 const False4=document.getElementById("False4");
 
-Croix1.addEventListener('click', function()
-{
-    Croix1.classList.add("dessin_croix");
-    
-})
 
-False1.addEventListener("click", function()
-{
-    Croix1.classList.add("dessin_croix");
-})
 
-Croix2.addEventListener('click', function()
-{
-    Croix2.classList.add("dessin_croix");
-});
 
-False2.addEventListener('click', function()
-{
-    Croix2.classList.add("dessin_croix");
-})
+
+
+
 
 Croix3.addEventListener("click", function()
 {
