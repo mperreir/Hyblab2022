b8ab8f86f5aade4e8f1a93f0f64aad7c0b951e1f
Gabriel JOLLY <gabriel-jolly@orange.fr>
2022-02-03 13:58:32 +0100

    changement question 7

--- communes-1/public/js/question.js
+++ communes-1/public/js/question.js
@@ -244,7 +244,7 @@ class Question7 extends Question{
 		let info_commune = data_election.get(key).get(this.code_commune);
 
 		let voteb_n1 = info_commune.pour_bl_nuls.vot.toFixed(2);
-		let voteb_n2 = parseInt(voteb_n1) + 3.5;
+		let voteb_n2 = parseInt(voteb_n1) + 1.5;
 
 		let question = "À " + info_commune.nom_com + ", quel est le pourcentage de votes blancs et " +
 			"nuls en " + this.annee + " au premier tour ?";
