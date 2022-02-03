689cfed3c5a63207da550d796aa2a09c4101bd9a
malomn <malo.maisonneuve@gmail.com>
2022-02-03 16:11:32 +0100

    Disabled swiper on first slides & timing of p4

--- pouvoirs/public/js/views/p4.js
+++ pouvoirs/public/js/views/p4.js
@@ -87,12 +87,12 @@ function linkDiv(divId, valid) {
                 setTimeout(()=>{
                     document.getElementById("p4-bulle-pm").setAttribute ('src',"img/dialogBoxes/P4_MEDIUM_NOTE.svg");
                     document.getElementById("p4p1").innerText = getText("p4-note1");
-                }, 2500);
+                }, 3000);
                 setTimeout(()=>{
                     document.getElementById("p4p1").innerText = getText("p4-note2");
                     swiper.enable();
                     showArrow();
-                }, 5000);
+                }, 7000);
             }
 
         }, false);
