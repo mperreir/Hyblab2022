b0679cd35a2936c6cdc3f6b16670b2fdded024d1
Thomas Clouet <thomas.clouet1@etu.univ-nantes.fr>
2022-02-03 08:19:09 +0100

    :zap: chargement random des personnages

---
+++ communes-1/public/js/js_anjoute-charger-personnage.js
@@ -0,0 +1,20 @@
+const video = document.querySelector('.animation_perso');
+
+let personnages = [
+	'img/animations/manteau_rouge.mp4',
+	'img/animations/manteau_bleu.mp4',
+	'img/animations/manteau_vert.mp4',
+	'img/animations/bonnie.mp4',
+	'img/animations/chapeau.mp4',
+	'img/animations/femme.mp4',
+	'img/animations/homme.mp4',
+	'img/animations/mamie.mp4',
+	'img/animations/rousse.mp4',
+	'img/animations/dame-reflet.mp4',
+]
+
+let source = document.createElement('source');
+source.setAttribute('src', personnages[localStorage.getItem('position_joueur')-1]);
+source.setAttribute('type', 'video/mp4');
+
+video.appendChild(source);
